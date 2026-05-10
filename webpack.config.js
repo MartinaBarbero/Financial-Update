f3c7d1adf9f53aa8259a895e0baef9f4fbb35f8b
Martina Barbero <MARTINA.BARBERO@UNIL.CH>
2026-05-10 14:43:06 +0200

    Add files via upload
    in client

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index.jsx",
+  output: { path: path.resolve(__dirname, "dist"), filename: "bundle.[contenthash].js", clean: true, publicPath: "/" },
+  resolve: { extensions: [".js", ".jsx"] },
+  module: {
+    rules: [
+      { test: /\.(js|jsx)$/, exclude: /node_modules/, use: { loader: "babel-loader", options: { presets: [["@babel/preset-env", { targets: { browsers: [">0.2%","not dead"] } }], ["@babel/preset-react", { runtime: "automatic" }]] } } },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] }
+    ]
+  },
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+  performance: { hints: false }
+};
